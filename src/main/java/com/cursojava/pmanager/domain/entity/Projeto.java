61571e172d68e9939a69ec8259f6203045b4a910
Fabio815 <fabiomotamarques815@gmail.com>
2026-07-08 21:02:57 -0300

    Dei uma comentada no código para não me perder

--- src/main/java/com/cursojava/pmanager/domain/entity/Projeto.java
+++ src/main/java/com/cursojava/pmanager/domain/entity/Projeto.java
@@ -7,11 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+//Usando lombok para tirar o BoilerPlate que é o código que não faz parte na regra de negócio, no caso não preciso dele para entender o fluxo do código (get, set, construtor, etc...).
+@Data //Basicamente aqui vou pré definir os gets, sets, hash, toSting etc...
+@Builder //Usado para facilitar a instância de objetos (facilita  muito).
+@AllArgsConstructor //Cria um construtur com elementos.
+@NoArgsConstructor //Construtur sem elementos
 public class Projeto {
     private String id;
     private String nome;
