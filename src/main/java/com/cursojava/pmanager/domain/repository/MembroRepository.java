029e79549428ff1a1980858ab2b4748edfad4b4b
Fabio815 <fabiomotamarques815@gmail.com>
2026-08-05 12:55:28 -0300

    Coloquei a anotação repository

--- src/main/java/com/cursojava/pmanager/domain/repository/MembroRepository.java
+++ src/main/java/com/cursojava/pmanager/domain/repository/MembroRepository.java
@@ -2,9 +2,10 @@ package com.cursojava.pmanager.domain.repository;
 
 import com.cursojava.pmanager.domain.entity.Membro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface MembroRepository extends JpaRepository<Membro, Long> {
 
     Optional<Membro> findByIdAndDesativo(Long id, boolean desativo);
